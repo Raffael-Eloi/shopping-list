d37d4ffc6ace111ffd127ddec86ef1f2037b5844
Raffael Eloi <raffaeleloi121@gmail.com>
2023-10-22 14:25:09 -0300

    add initial entities

---
+++ src/Domain/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Domain.Entities
+{
+    public class Product
+    {
+        public Product()
+        {
+            Id = Guid.NewGuid();
+            Reviews = new Collection<Review>();
+        }
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public int Quantity { get; set; }
+
+        public Promotion? Promotion { get; set; }
+
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
