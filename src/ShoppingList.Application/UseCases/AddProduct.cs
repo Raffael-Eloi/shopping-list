e38c7a6f1a430b254afbd45b17e27bd575697ba4
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-17 20:21:16 -0300

    refactor AddProduct

---
+++ src/ShoppingList.Application/UseCases/AddProduct.cs
@@ -0,0 +1,33 @@
+﻿using Application.Contracts;
+using Application.DTO;
+using Application.Models;
+using Domain.Contracts.Repositories;
+using Domain.Entities;
+
+namespace ShoppingList.Application.UseCases;
+
+public class AddProduct(IProductRepository productRepository) : IAddProduct
+{
+    public async Task<ProductResponse> AddAsync(RequestProductDTO addProductDTO)
+    {
+        Product product = CreateProduct(addProductDTO);
+
+        Guid productId = await productRepository.Add(product);
+
+        return new ProductResponse
+        {
+            ProductId = productId
+        };
+    }
+
+    private static Product CreateProduct(RequestProductDTO addProductDTO)
+    {
+        return new()
+        {
+            Name = addProductDTO.Name,
+            Description = addProductDTO.Description,
+            Price = addProductDTO.Price,
+            Quantity = addProductDTO.Quantity,
+        };
+    }
+}
