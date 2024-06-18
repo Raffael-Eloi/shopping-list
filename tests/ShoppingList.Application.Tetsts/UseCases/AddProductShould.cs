a0edce25561fee7a1a506114ed1e613785bed3d9
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-18 20:19:25 -0300

    Persist_When_Add_Product (GREEN)

--- tests/ShoppingList.Application.Tetsts/UseCases/AddProductShould.cs
+++ tests/ShoppingList.Application.Tetsts/UseCases/AddProductShould.cs
@@ -5,38 +5,38 @@ using ShoppingList.Application.DTO;
 using ShoppingList.Application.Models;
 using ShoppingList.Application.UseCases;
 using ShoppingList.Domain.Contracts.Repositories;
+using ShoppingList.Domain.Entities;
 
 namespace ShoppingList.Application.Tetsts.UseCases;
 
 internal class AddProductShould
 {
+    private IProductRepository productRepositoryMock;
+
+    private IAddProduct addProduct;
+    
+    private RequestProductDTO request;
+
+    [SetUp]
+    public void Setup()
+    {
+        productRepositoryMock = A.Fake<IProductRepository>();
+
+        addProduct = new AddProduct(productRepositoryMock);
+
+        request = new RequestProductDTO();
+    }
+
     [Test]
     public async Task Add_Product()
     {
         #region Arrange(Given)
 
-        string name = "Manchester City Shirt";
-        string description = "New manchester city shirt 2023/2024";
-        decimal price = 200;
-        int quantity = 10;
-
-        RequestProductDTO addProductDTO = new()
-        {
-            Name = name,
-            Description = description,
-            Price = price,
-            Quantity = quantity
-        };
-
-        var productRepositoryMock = A.Fake<IProductRepository>();
-
-        IAddProduct addProduct = new AddProduct(productRepositoryMock);
-
         #endregion
 
         #region Act(When)
 
-        ProductResponse response = await addProduct.AddAsync(addProductDTO);
+        ProductResponse response = await addProduct.AddAsync(request);
 
         #endregion
 
@@ -47,6 +47,48 @@ internal class AddProductShould
         response.IsValid.Should().BeTrue();
 
         response.ProductId.Should().NotBeNull();
+
+        #endregion
+    }
+    
+    [Test]
+    public async Task Persist_When_Add_Product()
+    {
+        #region Arrange(Given)
+
+        string name = "Manchester City Shirt";
+        
+        string description = "New manchester city shirt 2023/2024";
+        
+        decimal price = 200;
+        
+        int quantity = 10;
+
+        request.Name = name;
+        request.Description = description;
+        request.Price = price;
+        request.Quantity = quantity;
+
+        #endregion
+
+        #region Act(When)
+
+        ProductResponse response = await addProduct.AddAsync(request);
+
+        #endregion
+
+        #region Assert(Then)
+
+        A.CallTo(() => 
+            productRepositoryMock.Add(
+                A<Product>.That.Matches(
+                    product => 
+                        product.Name == name &&
+                        product.Description == description &&
+                        product.Price == price &&
+                        product.Quantity == quantity
+                    )))
+            .MustHaveHappenedOnceExactly();
 
         #endregion
     }
