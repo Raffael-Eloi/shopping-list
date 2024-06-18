f0a7eeac1e11c0d39a443d442837a439e7d5dd3f
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-18 20:33:14 -0300

    Return_Notification_When_Price_Is_Invalid (RED)

--- tests/ShoppingList.Application.Tetsts/Validators/AddProductValidatorShould.cs
+++ tests/ShoppingList.Application.Tetsts/Validators/AddProductValidatorShould.cs
@@ -6,19 +6,29 @@ namespace ShoppingList.Application.Tetsts.Validators;
 
 internal class AddProductValidatorShould
 {
+    private AddProductValidator validator;
+    
+    private AddProductRequest request;
+
+    [SetUp]
+    public void Setup()
+    {
+        validator = new AddProductValidator();
+
+        request = new AddProductRequest
+        {
+            Name = "name"
+        };
+    }
+
     [Test]
     public void Return_Notification_When_Name_Is_Empty()
     {
 		#region Arrange(Given)
 
-		string name = string.Empty;
+        string name = string.Empty;
 
-		var request = new AddProductRequest
-        {
-            Name = name
-        };
-
-        AddProductValidator validator = new AddProductValidator();
+        request.Name = name;
 
         #endregion
 
@@ -35,6 +45,34 @@ internal class AddProductValidatorShould
         result.IsValid.Should().BeFalse();
 
         result.Errors.First().ErrorMessage.Should().Be("'Name' must not be empty.");
+
+        #endregion
+    }
+    
+    [Test]
+    public void Return_Notification_When_Price_Is_Invalid()
+    {
+        #region Arrange(Given)
+
+        decimal invalidPrice = 0;
+
+        request.Price = invalidPrice;
+
+        #endregion
+
+        #region Act(When)
+
+        ValidationResult result = validator.Validate(request);
+
+        #endregion
+
+        #region Assert(Then)
+
+        result.Should().NotBeNull();
+
+        result.IsValid.Should().BeFalse();
+
+        result.Errors.First().ErrorMessage.Should().Be("'Price' must not be empty.");
 
         #endregion
     }
