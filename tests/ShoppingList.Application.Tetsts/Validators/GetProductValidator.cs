f87f49868c91e2efdb28d24e3cb861061b31fc93
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-18 22:59:56 -0300

    Return_Notification_When_Product_Does_Not_Exist (RED)

---
+++ tests/ShoppingList.Application.Tetsts/Validators/GetProductValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation.Results;
+using ShoppingList.Domain.Entities;
+
+namespace ShoppingList.Application.Tetsts.Validators
+{
+    internal class GetProductValidator
+    {
+        internal ValidationResult Validate(Product? unexistingProduct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/ShoppingList.Application.Tetsts/Validators/GetProductValidatorShould.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using FluentValidation.Results;
+using ShoppingList.Domain.Entities;
+
+namespace ShoppingList.Application.Tetsts.Validators;
+
+internal class GetProductValidatorShould
+{
+    [Test]
+    public void Return_Notification_When_Product_Does_Not_Exist()
+    {
+        #region Arrange(Given)
+
+        Product? unexistingProduct = null;
+
+        GetProductValidator validator = new GetProductValidator();
+
+        #endregion
+
+        #region Act(When)
+
+        ValidationResult result = validator.Validate(unexistingProduct);
+
+        #endregion
+
+        #region Assert(Then)
+
+        result.IsValid.Should().BeFalse();
+
+        result.Errors.First().ErrorMessage.Should().Be("'Price' must be greater than 0.");
+
+        #endregion
+    }
+}
