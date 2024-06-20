bc3fe8ef2f89a3c72f2d7ec19fe49a70d488cde2
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-20 18:54:27 -0300

    create template of BDD scenario of add new product

---
+++ tests/ShoppingList.Behavior.Tests/Features/AddProduct.feature
@@ -0,0 +1,10 @@
+﻿Feature: AddProduct
+
+Add a new product
+
+Scenario: Add new product
+	Given I want to add a new product
+	When I add the product with the following details
+		| Name   | Price | Description              | Quantity |
+		| Laptop | 5500  | A laptop with I5 8GB RAM | 10       |
+	Then the product should be added successfully
---
+++ tests/ShoppingList.Behavior.Tests/StepDefinitions/AddProductStepDefinitions.cs
@@ -0,0 +1,23 @@
+namespace ShoppingList.Behavior.Tests.StepDefinitions;
+
+[Binding]
+public class AddProductStepDefinitions
+{
+    [Given(@"I want to add a new product")]
+    public void GivenIWantToAddANewProduct()
+    {
+        throw new PendingStepException();
+    }
+
+    [When(@"I add the product with the following details")]
+    public void WhenIAddTheProductWithTheFollowingDetails(Table table)
+    {
+        throw new PendingStepException();
+    }
+
+    [Then(@"the product should be added successfully")]
+    public void ThenTheProductShouldBeAddedSuccessfully()
+    {
+        throw new PendingStepException();
+    }
+}
