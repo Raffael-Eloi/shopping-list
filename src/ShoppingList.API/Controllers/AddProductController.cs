b4d4555c57a786e6c87006757f84d1e8aac99dab
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-20 19:14:54 -0300

    create endpoint AddProductController

---
+++ src/ShoppingList.API/Controllers/AddProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingList.API.Extensions;
+using ShoppingList.Application.Contracts.UseCases;
+using ShoppingList.Application.Models;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace ShoppingList.API.Controllers;
+
+[Route("api/products")]
+[ApiController]
+public class AddProductController(IAddProduct addProduct) : ControllerBase
+{
+    [HttpGet(Name = "AddProduct")]
+    [SwaggerOperation(
+        Summary = "Add product",
+        Description = "Add a new product.")]
+    [ProducesResponseType(typeof(IEnumerable<AddProductResponse>),
+        StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ValidationProblemDetails),
+        StatusCodes.Status400BadRequest)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult> AddProduct(AddProductRequest request)
+    {
+        AddProductResponse response = await addProduct.AddAsync(request);
+
+        if (!response.IsValid)
+        {
+            return ValidationProblem(ModelState.AddErrosFromNofifications(response.Errors));
+        }
+
+        return StatusCode(StatusCodes.Status201Created, response);
+    }
+}
---
+++ src/ShoppingList.API/Extensions/AddErrosFromNotifications.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ShoppingList.API.Extensions;
+
+public static class AddErrosFromNotifications
+{
+    public static ModelStateDictionary AddErrosFromNofifications(this ModelStateDictionary modelState, IEnumerable<ValidationFailure> errors)
+    {
+        foreach (ValidationFailure item in errors)
+        {
+            modelState.AddModelError(item.ErrorCode, item.ErrorMessage);
+        }
+
+        return modelState;
+    }
+}
