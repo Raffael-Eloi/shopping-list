5761770680007871d32c28a689c4c693f5f22f40
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-19 21:01:16 -0300

    refactor GetProductValidator

--- tests/ShoppingList.Application.Tetsts/Validators/GetProductValidator.cs
+++ tests/ShoppingList.Application.Tetsts/Validators/GetProductValidator.cs
@@ -7,14 +7,24 @@ public class GetProductValidator : IGetProductValidator
 {
     public ValidationResult Validate(Product? product)
     {
-        if (product is null)
+        if (ProductDoesNExist(product))
         {
-            return new ValidationResult(new List<ValidationFailure>
-            {
-                new ValidationFailure("Product", "Product not found.")
-            });
+            return ValidationResultWithErrors();
         }
 
         return new ValidationResult();
+    }
+
+    private static ValidationResult ValidationResultWithErrors()
+    {
+        return new ValidationResult(new List<ValidationFailure>
+        {
+            new("Product", "Product not found.")
+        });
+    }
+
+    private static bool ProductDoesNExist(Product? product)
+    {
+        return product is null;
     }
 }
