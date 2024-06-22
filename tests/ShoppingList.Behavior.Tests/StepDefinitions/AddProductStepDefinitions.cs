7717a4ae3a9dd3d5c4a62c6236aa6b7d5e20ed2b
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-22 11:03:57 -0300

    create template of Behavior Tests integrate with open api

--- tests/ShoppingList.Behavior.Tests/StepDefinitions/AddProductStepDefinitions.cs
+++ tests/ShoppingList.Behavior.Tests/StepDefinitions/AddProductStepDefinitions.cs
@@ -1,23 +1,48 @@
+using TechTalk.SpecFlow.Assist;
+
 namespace ShoppingList.Behavior.Tests.StepDefinitions;
 
 [Binding]
-public class AddProductStepDefinitions
+public class AddProductStepDefinitions(
+    ScenarioContext context,
+    ShoppingListClient client)
 {
     [Given(@"I want to add a new product")]
-    public void GivenIWantToAddANewProduct()
+    public static void GivenIWantToAddANewProduct()
     {
-        throw new PendingStepException();
     }
 
     [When(@"I add the product with the following details")]
-    public void WhenIAddTheProductWithTheFollowingDetails(Table table)
+    public async Task WhenIAddTheProductWithTheFollowingDetails(Table table)
     {
-        throw new PendingStepException();
+        var request = table.CreateInstance<AddProductRequest>();
+
+        context.Set(request);
+
+        AddProductResponse response = await client.AddProductAsync(request);
+
+        context.Set(response);
     }
 
     [Then(@"the product should be added successfully")]
-    public void ThenTheProductShouldBeAddedSuccessfully()
+    public async Task ThenTheProductShouldBeAddedSuccessfully()
     {
-        throw new PendingStepException();
+        var response = context.Get<AddProductResponse>();
+        response.Should().NotBeNull();
+        response.IsValid.Should().BeTrue();
+        response.ProductId.Should().NotBeNull();
+
+        Guid productId = response.ProductId!.Value;
+
+        GetProductResponse product = await client.GetProductByIdAsync(productId);
+        product.Should().NotBeNull();
+        product.Id.Should().Be(productId);
+
+        var request = context.Get<AddProductRequest>();
+
+        product.Name.Should().Be(request.Name);
+        product.Description.Should().Be(request.Description);
+        product.Price.Should().Be(request.Price);
+        product.Quantity.Should().Be(request.Quantity);
     }
 }
