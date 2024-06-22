b7bcd6b7a5f9870f5995019bbf0f16ed7f121001
Raffael Eloi <raffaeleloi121@gmail.com>
2024-06-22 11:49:09 -0300

    add new product feature (RED)

---
+++ tests/ShoppingList.Behavior.Tests/Hooks/EnvironmentSetupHooks.cs
@@ -0,0 +1,27 @@
+﻿using BoDi;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ShoppingList.Behavior.Tests.Hooks;
+
+[Binding]
+public sealed class EnvironmentSetupHooks
+{
+    [BeforeTestRun]
+    public static void BeforeTestRun(IObjectContainer testThreadContainer)
+    {
+        HttpClient? apiHttpClient;
+
+        var application = new WebApplicationFactory<Program>()
+        .WithWebHostBuilder(builder =>
+        {
+            builder.UseEnvironment("Development");
+        });
+
+        apiHttpClient = application.CreateClient();
+
+        var shoppingListClient = new ShoppingListClient("", apiHttpClient);
+
+        testThreadContainer.RegisterInstanceAs(shoppingListClient);
+    }
+}
